9a346194eaa4b499ddcac61bf72f73dfe031a083
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-04-18 01:14:22 +0200

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# mymodule
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include mymodule
+class mymodule {
+}
---
+++ manifests/test.pp
@@ -0,0 +1,10 @@
+# mymodule::test
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include mymodule::test
+class mymodule::test {
+}
